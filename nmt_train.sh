7feafda8eb1e013701acc2ba0877773d489d4dc5
Kan Ren <rk6556@gmail.com>
2017-08-10 10:56:53 +0800

    add nmt training script.

---
+++ nmt_train.sh
@@ -0,0 +1,43 @@
+DATA_FOLD=${HOME}/DATA
+
+export VOCAB_SOURCE=$DATA_FOLD/nmt_data/toy_reverse/train/vocab.sources.txt
+export VOCAB_TARGET=$DATA_FOLD/nmt_data/toy_reverse/train/vocab.targets.txt
+export TRAIN_SOURCES=$DATA_FOLD/nmt_data/toy_reverse/train/sources.txt
+export TRAIN_TARGETS=$DATA_FOLD/nmt_data/toy_reverse/train/targets.txt
+export DEV_SOURCES=$DATA_FOLD/nmt_data/toy_reverse/dev/sources.txt
+export DEV_TARGETS=$DATA_FOLD/nmt_data/toy_reverse/dev/targets.txt
+
+export DEV_TARGETS_REF=$DATA_FOLD/nmt_data/toy_reverse/dev/targets.txt
+export TRAIN_STEPS=1000
+
+
+# TRAIN
+
+export MODEL_DIR=${TMPDIR:-/tmp}/nmt_tutorial
+mkdir -p $MODEL_DIR
+
+python -m bin.train \
+  --config_paths="
+      ./example_configs/nmt_small.yml,
+      ./example_configs/train_seq2seq.yml,
+      ./example_configs/text_metrics_bpe.yml" \
+  --model_params "
+      vocab_source: $VOCAB_SOURCE
+      vocab_target: $VOCAB_TARGET" \
+  --input_pipeline_train "
+    class: ParallelTextInputPipeline
+    params:
+      source_files:
+        - $TRAIN_SOURCES
+      target_files:
+        - $TRAIN_TARGETS" \
+  --input_pipeline_dev "
+    class: ParallelTextInputPipeline
+    params:
+       source_files:
+        - $DEV_SOURCES
+       target_files:
+        - $DEV_TARGETS" \
+  --batch_size 32 \
+  --train_steps $TRAIN_STEPS \
+  --output_dir $MODEL_DIR
